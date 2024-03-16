56217f73101c1d5572783b4b0b19ea3aa67b1e3d
DesiredName <wc3-enter@yandex.ru>
2024-03-16 22:38:57 +0100

    'commit'

---
+++ product-of-array-except-self/index.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+    const n = nums.length;
+    const ans = new Array(n);
+    for (let i = 0, left = 1; i < n; ++i) {
+        ans[i] = left;
+        left *= nums[i];
+    }
+    console.log(ans);
+    for (let i = n - 1, right = 1; i >= 0; --i) {
+        ans[i] *= right;
+        right *= nums[i];
+    }
+    return ans;
+};
+
+const tests = [
+    {
+        test: [1, 2, 3, 4],
+        expected: [24, 12, 8, 6],
+    },
+    {
+        test: [-1, 1, 0, -3, 3],
+        expected: [0, 0, 9, 0, 0],
+    },
+];
+
+for (const { test, expected } of tests) {
+    const result = productExceptSelf(test);
+    console.log({ test, result, expected });
+}
